9a643b49e595d152110f8e71ccb7b6ead2eb0ce9
amnar <amnarumaisa29@gmail.com>
2024-10-22 07:31:09 +0200

    created MareosSymptom class and SymptomFactory

--- src/domain/Covid19Pacient.java
+++ src/domain/Covid19Pacient.java
@@ -13,6 +13,7 @@ public class Covid19Pacient {
 	private String  name; 
 	private int age;
 	private Map<Symptom,Integer> symptoms=new HashMap<Symptom,Integer>();
+	SymptomFactory sf;
 
 	public Covid19Pacient(String name, int years) {
 		this.name = name;
@@ -35,9 +36,19 @@ public class Covid19Pacient {
 		return symptoms.get(s);
 	}
 
+	
+	
+	
+	
+	
 	public Set<Symptom> getSymptoms() {
 		return symptoms.keySet();
 	}
+	
+	
+	
+	
+	
 	
 	public Symptom getSymptomByName(String symptomName) {
 		Iterator<Symptom> i= getSymptoms().iterator();
@@ -48,6 +59,11 @@ public class Covid19Pacient {
 		}
 		return null;	
 	}
+	
+	
+	
+	
+	
 
 	public void addSymptom(Symptom c, Integer w){
 		symptoms.put(c,w);
@@ -56,11 +72,12 @@ public class Covid19Pacient {
 	public Symptom addSymptomByName(String symptom, Integer w){
 		Symptom s=getSymptomByName(symptom);
 		if (s==null) {
-			s=createSymptom(symptom); 
+			s=SymptomFactory.createSymptom(symptom); 
 			symptoms.put(s,w);		
 		}
 		return s;
 	}
+
 
 	public Symptom removeSymptomByName(String symptomName) {
 		Symptom s=getSymptomByName(symptomName);
@@ -68,6 +85,8 @@ public class Covid19Pacient {
 		if (s!=null) symptoms.remove(s);
 		return s;
 	}
+	
+	
 	public Iterator iterator() {
 		return new Covid19PacientIterator(this.symptoms.keySet());
 	}
@@ -92,32 +111,5 @@ public class Covid19Pacient {
 		return impact;
 	}
 	
-	private Symptom createSymptom(String symptomName) {
-	    List<String> impact5 = Arrays.asList("fiebre", "tos seca", "astenia","expectoracion");
-	    List<Double> index5 = Arrays.asList(87.9, 67.7, 38.1, 33.4);
-	    List<String> impact3 = Arrays.asList("disnea", "dolor de garganta", "cefalea","mialgia","escalofrios");
-	    List<Double> index3 = Arrays.asList(18.6, 13.9, 13.6, 14.8, 11.4);
-	    List<String> impact1 = Arrays.asList("nauseas", "vómitos", "congestión nasal","diarrea","hemoptisis","congestion conjuntival");
-	    List<Double> index1 = Arrays.asList(5.0, 4.8, 3.7, 0.9, 0.8);
-	    
-	    List<String> digestiveSymptom=Arrays.asList("nauseas", "vómitos","diarrea");
-	    List<String> neuroMuscularSymptom=Arrays.asList("fiebre", "astenia", "cefalea", "mialgia","escalofrios");
-	    List<String> respiratorySymptom=Arrays.asList("tos seca","expectoracion","disnea","dolor de garganta", "congestión nasal","hemoptisis","congestion conjuntival");
-
-
-	    int impact=0;
-	    double index=0;
-	    if (impact5.contains(symptomName)) {impact=5; index= index5.get(impact5.indexOf(symptomName));}
-	      else if (impact3.contains(symptomName)) {impact=3;index= index3.get(impact3.indexOf(symptomName));}
-	        else if (impact1.contains(symptomName)) {impact=1; index= index1.get(impact1.indexOf(symptomName));}
-	 
-	    if (impact!=0)  {
-	    	if (digestiveSymptom.contains(symptomName)) return new DigestiveSymptom(symptomName,(int)index, impact);
-	    	if (neuroMuscularSymptom.contains(symptomName)) return new NeuroMuscularSymptom(symptomName,(int)index, impact);
-	    	if (respiratorySymptom.contains(symptomName)) return new RespiratorySymptom(symptomName,(int)index, impact);
-	    }
-	    return null;		
-		
-	}
 }
 
